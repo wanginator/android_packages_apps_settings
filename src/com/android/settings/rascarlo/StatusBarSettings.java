f0a291397aadade27cbb222880a8d6a344da597b
rascarlo <rascarlo@gmail.com>
2013-08-07 10:05:51 +0200

    Don't expose quick pull down on tablet

--- src/com/android/settings/rascarlo/StatusBarSettings.java
+++ src/com/android/settings/rascarlo/StatusBarSettings.java
@@ -52,9 +52,10 @@ OnPreferenceChangeListener {
         } catch (SettingNotFoundException e) {
         }
 
-        // don't show status bar brightnees control on tablet
+        // don't show status bar brightnees and quick settings pull down control on tablet
         if (Utils.isTablet(getActivity())) {
             getPreferenceScreen().removePreference(mStatusBarBrightnessControl);
+            getPreferenceScreen().removePreference(mQuickPulldown);
         }
     }
 
