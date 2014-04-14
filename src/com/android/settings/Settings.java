d775c8b7494910a05e206974c0480505e575fdd9
Steve Kondik <shade@chemlab.org>
2014-04-13 23:37:43 -0700

    settings: Make mobile settings a first-class citizen
     * Nobody likes carriers, but let's not bury these settings behind
       10 layers of indirection. Many users (like me) who travel often
       will need to access these options frequently.
     * The top level switch disables mobile data.
    
    Change-Id: I1bd4d1116a2c73832230d66c94df2c3c0aacf698
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -91,6 +91,7 @@ import com.android.settings.mahdi.QuietHours;
 import com.android.settings.mahdi.themes.ThemeEnabler;
 import com.android.settings.mahdi.themes.ThemeSettings;
 import com.android.settings.mahdi.superuser.PolicyNativeFragment;
+import com.android.settings.net.MobileDataEnabler;
 import com.android.settings.nfc.AndroidBeam;
 import com.android.settings.nfc.PaymentSettings;
 import com.android.settings.print.PrintJobSettingsFragment;
@@ -601,6 +602,11 @@ public class Settings extends PreferenceActivity
                 if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)) {
                     target.remove(i);
                 }
+            } else if (id == R.id.mobile_network_settings) {
+                // Remove mobile network settings if the device doesn't have telephony
+                if (Utils.isWifiOnly(this)) {
+                    target.remove(i);
+                }
             } else if (id == R.id.data_usage_settings) {
                 // Remove data usage when kernel module not enabled
                 final INetworkManagementService netManager = INetworkManagementService.Stub
@@ -842,6 +848,7 @@ public class Settings extends PreferenceActivity
 
         private final WifiEnabler mWifiEnabler;
         private final BluetoothEnabler mBluetoothEnabler;
+        private final MobileDataEnabler mMobileDataEnabler;
         private final ProfileEnabler mProfileEnabler;
         private final VoiceWakeupEnabler mVoiceWakeupEnabler;
         public static ThemeEnabler mThemeEnabler;
@@ -864,6 +871,7 @@ public class Settings extends PreferenceActivity
                 return HEADER_TYPE_CATEGORY;
             } else if (header.id == R.id.wifi_settings
                     || header.id == R.id.bluetooth_settings
+                    || header.id == R.id.mobile_network_settings
                     || header.id == R.id.profiles_settings
                     || header.id == R.id.voice_wakeup_settings
                     || header.id == R.id.theme_settings_slim) {
@@ -912,6 +920,7 @@ public class Settings extends PreferenceActivity
             // Switches inflated from their layouts. Must be done before adapter is set in super
             mWifiEnabler = new WifiEnabler(context, new Switch(context));
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
+            mMobileDataEnabler = new MobileDataEnabler(context, new Switch(context));
             mProfileEnabler = new ProfileEnabler(context, new Switch(context));
             mVoiceWakeupEnabler = new VoiceWakeupEnabler(context, new Switch(context));
             mThemeEnabler = new ThemeEnabler(context, new Switch(context));
@@ -986,6 +995,8 @@ public class Settings extends PreferenceActivity
                         mWifiEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.bluetooth_settings) {
                         mBluetoothEnabler.setSwitch(holder.switch_);
+                    } else if (header.id == R.id.mobile_network_settings) {
+                        mMobileDataEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.profiles_settings) {
                         mProfileEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.voice_wakeup_settings) {
@@ -1065,6 +1076,7 @@ public class Settings extends PreferenceActivity
         public void resume() {
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
+            mMobileDataEnabler.resume();
             mProfileEnabler.resume();
             mVoiceWakeupEnabler.resume();
             mThemeEnabler.resume();
@@ -1073,6 +1085,7 @@ public class Settings extends PreferenceActivity
         public void pause() {
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
+            mMobileDataEnabler.pause();
             mProfileEnabler.pause();
             mVoiceWakeupEnabler.pause();
             mThemeEnabler.pause();
