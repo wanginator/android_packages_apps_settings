45e2201e3444ff92a4429f5fa104e870baf2db6b
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-04-23 14:01:26 +0100

    Power menu settings: clean up
    -add back immersive mode option
    -remove profile option to show on lockscreen

--- src/com/android/settings/mahdi/PowerMenu.java
+++ src/com/android/settings/mahdi/PowerMenu.java
@@ -16,44 +16,26 @@
 
 package com.android.settings.mahdi;
 
-import android.content.res.Resources;
-import android.os.RemoteException;
+import android.content.ContentResolver;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.CheckBoxPreference;
-import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
-import android.util.Log;
-import android.view.WindowManagerGlobal;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.android.internal.util.mahdi.DeviceUtils;
 
-public class PowerMenu extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+public class PowerMenu extends SettingsPreferenceFragment {
     private static final String TAG = "PowerMenu";
 
-    private static final String KEY_REBOOT = "power_menu_reboot";    
-    private static final String KEY_PROFILES = "power_menu_profiles";
-    private static final String KEY_SCREENSHOT = "power_menu_screenshot";
-    private static final String KEY_SCREENRECORD = "power_menu_screenrecord";
     private static final String KEY_ONTHEGO = "power_menu_onthego_enabled";
-    private static final String KEY_AIRPLANE = "power_menu_airplane";
-    private static final String KEY_SILENT = "power_menu_silent";
 
-    private CheckBoxPreference mRebootPref;
-    private ListPreference mProfilesPref;
-    private CheckBoxPreference mScreenshotPref;
-    private CheckBoxPreference mScreenrecordPref;
     private CheckBoxPreference mOnthegoPref;
-    private CheckBoxPreference mAirplanePref;
-    private CheckBoxPreference mSilentPref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,92 +43,33 @@ public class PowerMenu extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.power_menu_settings);
 
-        PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getContentResolver();
 
+        // Only enable on the go item if device has camera
         findPreference(Settings.System.POWER_MENU_ONTHEGO_ENABLED).setEnabled(
                 DeviceUtils.hasCamera(getActivity()));
-
-        mRebootPref = (CheckBoxPreference) findPreference(KEY_REBOOT);
-        mRebootPref.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.POWER_MENU_REBOOT_ENABLED, 1) == 1));
-
-	mProfilesPref = (ListPreference) findPreference(KEY_PROFILES);
-        mProfilesPref.setOnPreferenceChangeListener(this);
-        int mProfileShow = Settings.System.getInt(getContentResolver(),
-                Settings.System.POWER_MENU_PROFILES_ENABLED, 1);
-        mProfilesPref.setValue(String.valueOf(mProfileShow));
-        mProfilesPref.setSummary(mProfilesPref.getEntries()[mProfileShow]);
-        // Only enable if System Profiles are also enabled
-        boolean enabled = Settings.System.getInt(getContentResolver(),
-                Settings.System.SYSTEM_PROFILES_ENABLED, 1) == 1;
-        mProfilesPref.setEnabled(enabled);
-
-        mScreenshotPref = (CheckBoxPreference) findPreference(KEY_SCREENSHOT);
-        mScreenshotPref.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.POWER_MENU_SCREENSHOT_ENABLED, 0) == 1));
-
-	mScreenrecordPref = (CheckBoxPreference) findPreference(KEY_SCREENRECORD);
-        mScreenrecordPref.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.POWER_MENU_SCREENRECORD_ENABLED, 0) == 1));
 
         mOnthegoPref = (CheckBoxPreference) findPreference(KEY_ONTHEGO);
         mOnthegoPref.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.POWER_MENU_ONTHEGO_ENABLED, 0) == 1));
 
-        mAirplanePref = (CheckBoxPreference) findPreference(KEY_AIRPLANE);
-        mAirplanePref.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.POWER_MENU_AIRPLANE_ENABLED, 1) == 1));                
+        // Only enable expanded desktop item if expanded desktop support is also enabled
+        findPreference(Settings.System.POWER_MENU_GLOBAL_IMMERSIVE_MODE_ENABLED).setEnabled(
+                Settings.System.getInt(resolver, Settings.System.GLOBAL_IMMERSIVE_MODE_STYLE, 0) != 0);
 
-        mSilentPref = (CheckBoxPreference) findPreference(KEY_SILENT);
-        mSilentPref.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.POWER_MENU_SILENT_ENABLED, 1) == 1));
-    } 
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-       if (preference == mProfilesPref) {
-            int mProfileShow = Integer.valueOf((String) newValue);
-            int index = mProfilesPref.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.POWER_MENU_PROFILES_ENABLED, mProfileShow);
-            mProfilesPref.setSummary(mProfilesPref.getEntries()[index]);
-            return true;
-        }
-        return false;
-    }       
+        // Only enable profiles item if System Profiles are also enabled
+        findPreference(Settings.System.POWER_MENU_PROFILES_ENABLED).setEnabled(
+                Settings.System.getInt(resolver, Settings.System.SYSTEM_PROFILES_ENABLED, 1) != 0);
+    }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
 
-        if (preference == mScreenshotPref) {
-            value = mScreenshotPref.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.POWER_MENU_SCREENSHOT_ENABLED,
-                    value ? 1 : 0);
-	} else if (preference == mScreenrecordPref) {
-            value = mScreenrecordPref.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.POWER_MENU_SCREENRECORD_ENABLED,
-                    value ? 1 : 0);
-        } else if (preference == mOnthegoPref) {
+        if (preference == mOnthegoPref) {
             value = mOnthegoPref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.POWER_MENU_ONTHEGO_ENABLED,
-                    value ? 1 : 0);      
-        } else if (preference == mRebootPref) {
-            value = mRebootPref.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.POWER_MENU_REBOOT_ENABLED,
-                    value ? 1 : 0);        
-       } else if (preference == mAirplanePref) {
-            value = mAirplanePref.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.POWER_MENU_AIRPLANE_ENABLED,
-                    value ? 1 : 0);       
-       } else if (preference == mSilentPref) {
-            value = mSilentPref.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.POWER_MENU_SILENT_ENABLED,
                     value ? 1 : 0);
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
